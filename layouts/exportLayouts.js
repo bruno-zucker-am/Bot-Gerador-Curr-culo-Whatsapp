87b44ef7556c18f7365d15c74b79ac999ab5fb87
Bruno Zucker <bruno.zucker.am@gmail.com>
2026-03-22 16:13:37 -0400

    Update exportLayouts.js

--- layouts/exportLayouts.js
+++ layouts/exportLayouts.js
@@ -1,1 +1,9 @@
+const layout1 = require('./layout1');
+const layout2 = require('./layout2');
+const layout3 = require('./layout3');
 
+module.exports = {
+    layout1,
+    layout2,
+    layout3
+};
